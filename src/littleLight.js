05fe78a122b091de79b7763cdfb3879e441f9d24
Ethan Sawyer <ethan@ehsawyer.uk>
2019-07-25 09:51:47 +0100

    Stopped Exposing Higher Guardian Class

--- src/littleLight.js
+++ src/littleLight.js
@@ -50,23 +50,31 @@ class Warlock extends Guardian {
      * Create a Warlock
      * @param {number} power The Warlock's power/light level
      */
-    constructor(power) { super("Warlock", power) }
+    constructor(power) {
+        super("Warlock", power)
+    }
 
     /**
      * Get the Warlock's archetype/class
      * @return {string} The Guardian's archetype/class
      */
-    get archetype() { return super.archetype }
+    get archetype() {
+        return super.archetype
+    }
     /**
      * Get the Warlock's power/light level
      * @return {number} The Guardian's power/light level, rounded down to the nearest multiple of 5
      */
-    get power() { return super.power }
+    get power() {
+        return super.power
+    }
     /**
      * Get the Warlock's power/light level
      * @return {string} The Guardian's power/light level
      */
-    get powerRaw() { return super.powerRaw }
+    get powerRaw() {
+        return super.powerRaw
+    }
 }
 
 class Hunter extends Guardian {
@@ -74,23 +82,31 @@ class Hunter extends Guardian {
      * Create a Hunter
      * @param {number} power The Hunter's power/light level
      */
-    constructor(power) { super("Hunter", power) }
+    constructor(power) {
+        super("Hunter", power)
+    }
 
     /**
      * Get the Hunter's archetype/class
      * @return {string} The Guardian's archetype/class
      */
-    get archetype() { return super.archetype }
+    get archetype() {
+        return super.archetype
+    }
     /**
      * Get the Hunter's power/light level
      * @return {number} The Guardian's power/light level, rounded down to the nearest multiple of 5
      */
-    get power() { return super.power }
+    get power() {
+        return super.power
+    }
     /**
      * Get the Hunter's power/light level
      * @return {string} The Guardian's power/light level
      */
-    get powerRaw() { return super.powerRaw }
+    get powerRaw() {
+        return super.powerRaw
+    }
 }
 
 class Titan extends Guardian {
@@ -98,26 +114,33 @@ class Titan extends Guardian {
      * Create a Titan
      * @param {number} power The Titan's power/light level
      */
-    constructor(power) { super("Titan", power) }
+    constructor(power) {
+        super("Titan", power)
+    }
 
     /**
      * Get the Titan's archetype/class
      * @return {string} The Guardian's archetype/class
      */
-    get archetype() { return super.archetype }
+    get archetype() {
+        return super.archetype
+    }
     /**
      * Get the Titan's power/light level
      * @return {number} The Guardian's power/light level, rounded down to the nearest multiple of 5
      */
-    get power() { return super.power }
+    get power() {
+        return super.power
+    }
     /**
      * Get the Titan's power/light level
      * @return {string} The Guardian's power/light level
      */
-    get powerRaw() { return super.powerRaw }
+    get powerRaw() {
+        return super.powerRaw
+    }
 }
 
-module.exports.Guardian = Guardian;
 module.exports.Warlock = Warlock;
 module.exports.Hunter = Hunter;
 module.exports.Titan = Titan;
